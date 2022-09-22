ce800220901f150b0e53cf153eadaff97bbd0da8
unknown <koustubhdube@outlook.com>
2022-09-22 12:56:53 +0200

    Added about me and theme button

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('Hello')
+let body= document.getElementById('bodyId');
+let button= document.getElementById('buttonTheme')
+function themeChange(){
+    console.log(body)
+    if(button.innerHTML === 'Dark'){
+        button.innerHTML="Light"
+    }
+    else if (button.innerHTML === 'Light'){
+        button.innerHTML="Dark"
+    }
+    
+    body.classList.toggle('bodyClassDark');
+    body.classList.toggle('bodyClassLight');
+}
