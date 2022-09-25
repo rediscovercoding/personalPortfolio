ef5b1064ca8baba9c6b3c2ad7823059c08f96d89
unknown <koustubhdube@outlook.com>
2022-09-25 07:12:45 +0200

    Add resposive design for lower than 600px

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ let body= document.getElementById('bodyId');
 let button= document.getElementById('buttonTheme')
 function themeChange(){
     console.log(body)
-    if(button.innerHTML === 'Dark'){
-        button.innerHTML="Light"
+    if(button.innerHTML === 'Dark Theme'){
+        button.innerHTML="Light Theme"
     }
-    else if (button.innerHTML === 'Light'){
-        button.innerHTML="Dark"
+    else if (button.innerHTML === 'Light Theme'){
+        button.innerHTML="Dark Theme"
     }
     
     body.classList.toggle('bodyClassDark');
     body.classList.toggle('bodyClassLight');
+}
+
+
+let contactContainer= document.getElementById('contactContainer')
+function closePopUp(){
+    contactContainer.style.display= 'none';
+}
+
+function contactPopUp(){
+    contactContainer.style.display= 'grid';
 }
